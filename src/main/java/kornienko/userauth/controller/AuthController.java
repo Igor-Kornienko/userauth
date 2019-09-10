156f86307584e4a057dace807abaf2b63a58ff0d
igor-kornienko <igor.kornienko@computools.com>
2019-09-10 13:03:37 +0300

    Initial commit

---
+++ src/main/java/kornienko/userauth/controller/AuthController.java
@@ -0,0 +1,14 @@
+package kornienko.userauth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class AuthController {
+    @RequestMapping(value = "/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+}
